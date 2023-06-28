6ecbeca9c62165e4aaac0e8ee948190b67553aeb
cgrams <cgrams2@uic.edu>
2023-06-28 14:38:20 -0500

    Cleaned up rendering.py

--- src/rendering.py
+++ src/rendering.py
@@ -1,8 +1,18 @@
+import numpy as np
 import pymol  # must be installed on system (see README.md)
+import re
+import time
+
+from collections import defaultdict
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.database import Base, Job, SequenceCoverageResult
 from vendor.python.pymol2glmol import *
 
 
-def setup_pymol(pdb_file, pdb_name):
+def setup_pymol_from_file(pdb_file, pdb_name):
     # Load the PDB file
     pymol.pymol_argv = ['pymol', '-qc']  # pymol launching: quiet (-q), without GUI (-c)
     pymol.finish_launching()
@@ -15,37 +25,167 @@ def setup_pymol(pdb_file, pdb_name):
     pymol.cmd.bg_color('black')
 
 
-def get_objs(name):
+def get_pdb_str_from_file(pdb_file):
+    with open(pdb_file, 'r') as f:
+        pdb_str = f.read()
+    return pdb_str
+
+
+def setup_pymol_from_string(pdb_str, pdb_name):
+    # Load the PDB file from string
+    pymol.pymol_argv = ['pymol', '-qc']  # pymol launching: quiet (-q), without GUI (-c)
+    pymol.finish_launching()
+    pymol.cmd.read_pdbstr(pdb_str, pdb_name)
+    pymol.cmd.disable("all")
+    pymol.cmd.enable()
+    pymol.cmd.hide('all')
+    pymol.cmd.show('cartoon')
+    pymol.cmd.set('ray_opaque_background', 0)
+    pymol.cmd.bg_color('black')
+
+
+def color_getter(sequence_coverage: list,
+                 ptms: dict,
+                 ptm_annotations: dict,
+                 pdb_str: str) -> str:
+    # from pdb_str get element pos to amino acid pos
+    pdb_str = pdb_str.split('\nTER')[0].split('\n')
+    amino_ele_pos_dict = defaultdict(list)
+    for line in pdb_str:
+        amino_ele_pos_dict[int(re.search('\d+(?=\s+[+-]?\d+\.)', line).group())].append(
+            int(re.search('\d+', line).group()))
+    amino_ele_pos_dict = {each: sorted(amino_ele_pos_dict[each]) for each in amino_ele_pos_dict}
+    for kpos in range(len(sequence_coverage)):
+        if kpos + 1 not in amino_ele_pos_dict:
+            amino_ele_pos_dict[kpos + 1] = [0]
+    amino_ele_pos_dict = {each: sorted(amino_ele_pos_dict[each]) for each in amino_ele_pos_dict}
+
+    default = 'color:0.863,0.863,0.863:'  # grey
+    covered = 'color:1.000,0.243,0.243:'
+    # get index of zeros and nonzeros
+    sequence_coverage = np.array(seq_result.sequence_coverage).astype(np.int32)  # convert to numpy array
+    non_zero_index = np.nonzero(sequence_coverage)[0]
+    zero_index = np.nonzero(sequence_coverage == 0)[0]
+
+    # get index blocks of zeros and nonzeros
+    cov_pos_block = np.split(non_zero_index, np.where(np.diff(non_zero_index) != 1)[0] + 1)
+    non_cov_pos_block = np.split(zero_index, np.where(np.diff(zero_index) != 1)[0] + 1)
+
+    # string concatenate
+    default += ','.join([str(amino_ele_pos_dict[each[0] + 1][0]) + '-' + str(amino_ele_pos_dict[each[-1] + 1][-1])
+                         for each in non_cov_pos_block])
+    covered += ','.join([str(amino_ele_pos_dict[each[0] + 1][0]) + '-' + str(amino_ele_pos_dict[each[-1] + 1][-1])
+                         for each in cov_pos_block])
+
+    # ptm color string concatenate
+    ptm_color = ''
+    if ptms:
+        for ptm in ptms:
+            ptm_color += 'color:' + ','.join(
+                ['%.3f' % (int(each) / 256) for each in ptm_annotations[ptm]]) + ':'
+            ptm_color += ','.join([str(amino_ele_pos_dict[idx + 1][0]) + '-'
+                                   + str(amino_ele_pos_dict[idx + 1][-1])
+                                   for idx in ptms[ptm]])
+            ptm_color += '\n'
+    return default + '\n' + covered + '\n' + ptm_color.rstrip('\n')
+
+
+def get_objs(pdb_name: str) -> str:
     try:
         cmd.set('pse_export_version', 1.74)
-    except:
-        pass
+    except pymol.CmdException as e:
+        print(f'A pymol error occurred: {e}')
     names = cmd.get_session()['names']
     cmd.set('pdb_retain_ids', 1)
 
     ret = ''
 
     for obj in names:
-        if obj == None:
+        if obj is None:
             continue
         if obj[2] == 0:  # not visible
             continue
-        if obj[1] == 0 and obj[4] == 1 and obj[0] == name:
+        if obj[1] == 0 and obj[4] == 1 and obj[0] == pdb_name:
             ret += parseObjMol(obj)
-            print(ret)
         if obj[1] == 0 and obj[4] == 4:  # currently all dist objects are exported
             ret += parseDistObj(obj)
+
+    ret += '\n'
 
     return ret
 
 
-def show_cov_3D(pdb_file, pdb_name):
-    setup_pymol(pdb_file, pdb_name)
-    ret = get_objs(pdb_name)
-    print(ret)
+def get_view() -> str:
+    ret = ''
+    cmd.turn('z', 180)
+    view = cmd.get_view()
+    cmd.turn('z', 180)
+    cx = -view[12]
+    cy = -view[13]
+    cz = -view[14]
+    cameraZ = - view[11] - 150
+    fov = float(cmd.get("field_of_view"))
+    fogStart = float(cmd.get("fog_start"))
+    slabNear = view[15] + view[11]
+    slabFar = view[16] + view[11]
+    ret += "\nview:%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f" % \
+           (cx, cy, cz, cameraZ, slabNear, slabFar, fogStart, fov)
+    for i in range(9):
+        ret += ",%.3f" % view[i]
+
+    ret += '\n'
+
+    return ret
+
+
+def show_cov_3D(pdb_file: str,
+                pdb_name: str,
+                sequence_coverage: list,
+                ptms: dict,
+                ptm_annotations: dict,
+                background_color: int) -> str:
+    """
+    Generate GLmol string for 3D visualization of protein coverage
+    """
+    setup_pymol_from_file(pdb_file, pdb_name)  # setup pymol + load pdb
+    pdb_str = cmd.get_pdbstr(pdb_name)  # get pdb string
+
+    ret = get_objs(pdb_name) \
+          + color_getter(sequence_coverage, ptms, ptm_annotations, pdb_str) \
+          + get_view() \
+          + f"bgcolor:{background_color}"
+
+    return ret
 
 
 if __name__ == "__main__":
-    pdb_file = "C:\\Users\\Chris\\Downloads\\protein-vis\\Protein-Vis\\pdbs\\UP000000589_10090_MOUSE\\AF-Q99JR5-F1-model_v1.pdb"
+    pdb_file = "C:\\Users\\cgrams\\protein-vis\\Protein-Vis\\pdbs\\UP000000589_10090_MOUSE\\AF-Q99JR5-F1-model_v1.pdb"
     pdb_name = "AF-Q99JR5-F1-model_v1.pdb"
-    show_cov_3D(pdb_file, pdb_name)
+
+    db_start = time.time()
+    engine = create_engine('sqlite:///../db/dev.db')  # create SQLAlchemy engine
+
+    Base.metadata.create_all(engine)  # create database tables
+
+    Session = sessionmaker(bind=engine)  # create session factory
+
+    session = Session()
+
+    job = session.query(Job).filter(Job.job_number == '03028b4d-f9d4-4a20-893c-fb344d6441e6').first()
+
+    seq_result = session.query(SequenceCoverageResult).filter(SequenceCoverageResult.protein_id == "Q99JR5").first()
+
+    print(time.time() - db_start)
+
+    start = time.time()
+    ret = show_cov_3D(pdb_file,
+                      pdb_name,
+                      seq_result.sequence_coverage,
+                      seq_result.ptms,
+                      job.ptm_annotations,
+                      job.background_color
+                      )
+
+    print(ret)
+
+    print(time.time() - start)
