c9110faa3d97c3179dbe1e05d9d4416c7a0daaae
cgrams <cgrams2@uic.edu>
2023-07-03 15:45:14 -0500

    Used multiprocessing in process_pdb.py

--- generate/process_pdb.py
+++ generate/process_pdb.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 import argparse
 from dotenv import load_dotenv
@@ -9,48 +10,46 @@ from src.rendering import get_db_model_from_pdb
 
 load_dotenv('../.env')  # load environmental variables from .env
 
-engine = create_engine(os.getenv('DATABASE_URL'))  # create SQLAlchemy engine
+parser = argparse.ArgumentParser(description="Process PDB files into database.")
+parser.add_argument('-d', '--directory', help="Directory of PDB files", required=True)
+parser.add_argument('-s', '--species', help="Desired species.", required=True)
+parser.add_argument('--database', help="Database to add to.", default=os.getenv('DATABASE_URL'), required=False)
 
-Base.metadata.create_all(engine)  # create database tables
+args = parser.parse_args()
 
-Session = sessionmaker(bind=engine)  # create session factory
+
+def process_pdb_file(pdb_file):
+    # Create a new session for this process
+    engine = create_engine(os.getenv('DATABASE_URL'))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    mdl = get_db_model_from_pdb(pdb_file, os.path.basename(pdb_file), os.path.basename(pdb_file).split('-')[1], args.species)
+
+    # Check if entry already exists
+    existing_entry = session.query(ProteinStructure).filter_by(id=mdl.id).first()
+    if existing_entry:
+        session.close()
+        return f"Entry with id \"{mdl.id}\" already exists in database."
+    else:
+        session.merge(mdl) # merge the object into the session
+        session.commit()
+        session.close()
+        return f"Added {pdb_file} to database as \"{mdl.id}.\""
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Process PDB files into database.")
-    parser.add_argument('-d', '--directory', help="Directory of PDB files", required=True)
-    parser.add_argument('-s', '--species', help="Desired species.", required=True)
-    parser.add_argument('--database', help="Database to add to.", default=os.getenv('DATABASE_URL'), required=False)
-
-    args = parser.parse_args()
-
-    session = Session()
-    # existing_entry = session.query(ProteinStructure).filter_by(name=args.name).first()
-    # if existing_entry:
-    #     print(f"Entry with name \"{args.name}\" already exists in database.")
-    #     return
-
     # Get all PDB files in directory
     pdb_files = [os.path.join(args.directory, f) for f in os.listdir(args.directory) if f.endswith('.pdb')]
     if len(pdb_files) == 0:
         print(f"No PDB files found in {args.directory}.")
         return
 
-    # Render 3D Structures
-    for pdb_file in tqdm(pdb_files, desc="Processing files", unit="file"):
-        mdl = get_db_model_from_pdb(pdb_file, os.path.basename(pdb_file), os.path.basename(pdb_file).split('-')[1], args.species)
-
-        # Check if entry already exists
-        existing_entry = session.query(ProteinStructure).filter_by(id=mdl.id).first()
-        if existing_entry:
-            print(f"Entry with id \"{mdl.id}\" already exists in database.")
-            continue
-        else:
-            session.add(mdl)
-            session.commit()
-            print(f"Added {pdb_file} to database as \"{mdl.id}.\"")
-
-    session.close()
+    # Use a multiprocessing Pool
+    with multiprocessing.Pool() as pool:
+        for message in tqdm(pool.imap_unordered(process_pdb_file, pdb_files), total=len(pdb_files),
+                            desc="Processing files", unit="file"):
+            print(message)
 
 
 if __name__ == "__main__":
